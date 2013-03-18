377e21b46df5817228f3a6e3bea45f5b301ca127
Xavier Decoster <xavier.decoster@gmail.com>
2013-03-18 20:22:19 +0100

    add test to be fixed

--- src/Extension/NuGet.PackageSourceDiscovery.Extension.Tests/Commands/DiscoverCommandTest.cs
+++ src/Extension/NuGet.PackageSourceDiscovery.Extension.Tests/Commands/DiscoverCommandTest.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+﻿using System;
+using Moq;
+using Xunit;
 using Xunit.Extensions;
 
 namespace NuGet.Test.Commands
@@ -33,5 +35,21 @@ namespace NuGet.Test.Commands
             // Act and Assert
             ExceptionAssert.Throws<CommandLineException>(discoverCommand.ExecuteCommand, "The URL specified is invalid. Please provide a valid URL.");
         }
+
+        //[Theory]
+        //[InlineData("www.xavierdecoster.com")]
+        //[InlineData("http://www.xavierdecoster.com")]
+        //public void CommandDoesNotThrowIfUrlIsValidUrl(string url)
+        //{
+        //    // Arrange
+        //    var packageSourceProvider = new Mock<IPackageSourceProvider>(MockBehavior.Strict);
+        //    var discoverCommand = new DiscoverCommandMock();
+        //    discoverCommand.SetPackageSourceProvider(packageSourceProvider.Object);
+        //    discoverCommand.Url = url;
+        //    Action act = discoverCommand.ExecuteCommand;
+
+        //    // Act and Assert
+        //    Assert.DoesNotThrow(() => act());
+        //}
     }
 }
